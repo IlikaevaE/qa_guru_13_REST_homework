26c40b4d4d6a43d5b2a3ec5e88431d5b4b41103e
PC <gud_135win@mail.ru>
2022-08-07 13:32:15 +0300

    updated

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -30,8 +30,8 @@ public class TestBase {
 
         RestAssured.baseURI = "http://demowebshop.tricentis.com";
         Configuration.baseUrl = "http://demowebshop.tricentis.com";
-   Configuration.remote = String.format("https://%s:%s@%s", config.login(), config.password(), config.remoteWD());
-      //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+  // Configuration.remote = String.format("https://%s:%s@%s", config.login(), config.password(), config.remoteWD());
+     Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
     }
 
